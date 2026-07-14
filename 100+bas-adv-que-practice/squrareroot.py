57e2460fa312fa3a6224aac37e5be0d4e67b8cab
sonali637 <sonalibiswal108@gmail.com>
2026-07-14 13:27:20 +0530

    square root

---
+++ 100+bas-adv-que-practice/squrareroot.py
@@ -0,0 +1,18 @@
+# que no2 :- find the square root of the number
+# here we also hove 2methods to solve this problem
+# 1. using exponention method
+# formula of finding square root of some number is number x (1/2) or (0.5)
+num=64
+sq = num**(1/2)
+print("square root of the number is:",sq)
+
+# using num x (0.5)
+number = 98
+sq2 = number**(0.5)
+print(sq2)
+
+# using meth module
+import math
+no = 42
+sr = math.sqrt(no)
+print(sr)
