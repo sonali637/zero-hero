d0ca2a06546e69dc186474a6ffa352ad1ad1048e
sonali637 <sonalibiswal108@gmail.com>
2026-07-14 12:47:39 +0530

    add two number

---
+++ 100+bas-adv-que-practice/add2num-.py
@@ -0,0 +1,17 @@
+# que:- add two number using python program
+# here we can do this program using 2 methods 
+# 1. using predicting variable
+num1 = 34
+num2 = 66
+sum = (num1+num2)
+print(sum)
+
+# we also do this thing in another way
+nu1 = 24
+nu2 = 34
+print(nu1+nu2)
+
+# 2. user input
+n1 = int(input("enter your no1: "))
+n2 =int(input("enter your no2: "))
+print(n1+n2)
