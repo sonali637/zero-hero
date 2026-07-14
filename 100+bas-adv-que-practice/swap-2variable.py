df1373ca1fa52157b37e3c3213b7bcf5b0bb5814
sonali637 <sonalibiswal108@gmail.com>
2026-07-14 14:22:20 +0530

    swap two variable

---
+++ 100+bas-adv-que-practice/swap-2variable.py
@@ -0,0 +1,15 @@
+# que no4:- swap two variable
+# in this problem we also use 2 method to swap two varioable
+# using temp variable
+x = 38
+y = 98
+temp = x
+x = y
+y = temp
+print (x,y,temp)
+
+# with out using temp variable
+a = 80
+b = 65
+a,b = b,a
+print(a,b)
