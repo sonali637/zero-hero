8aa0a2d3131eedc780e8b929f2e75667499cff11
sonali637 <sonalibiswal108@gmail.com>
2026-07-14 14:06:32 +0530

    area of the triangale

---
+++ 100+bas-adv-que-practice/area-of-triangle.py
@@ -0,0 +1,11 @@
+# que no3:- calcule the area of the triangele
+# formula to find the area of the triangale (1/2)xhight x base
+h = 7
+b= 6
+area = (1/2)*h*b
+print(area)
+# another way
+hight = float(input("enter the hight of the triangle:"))
+base = float(input("enter the base of the triangle:"))
+area=(0.5)*hight*base
+print(area)
