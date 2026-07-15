33fd6a79bc6ed917203086ddace8a428506677e8
sonali637 <sonalibiswal108@gmail.com>
2026-07-15 22:56:31 +0530

    find number is negative or positive or zero

---
+++ 100+bas-adv-que-practice/+ve_-ve_Zero.py
@@ -0,0 +1,8 @@
+# que no7:- check number are positive,negative or zero
+num = int(input("enter the number:"))
+if (num > 0):
+    print("number is positive")
+elif (num < 0):
+    print("number is negative")
+else:
+    print("number is zero")        
